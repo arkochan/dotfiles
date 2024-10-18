052993c5c768b5a30dd77274500700008040734f
arkochan <najmussakib@iut-dhaka.edu>
2024-10-18 16:45:35 +0600

    conda

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,17 @@ end
 
 #setup zoxide
 zoxide init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/arkochan/miniconda3/bin/conda
+    eval /home/arkochan/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/arkochan/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/arkochan/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/arkochan/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
