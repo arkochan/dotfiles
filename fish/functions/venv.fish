94d207daad32bf48a1b314877857668c92556aa5
arkochan <najmussakib@iut-dhaka.edu>
2024-10-18 16:45:10 +0600

    venv script

---
+++ fish/functions/venv.fish
@@ -0,0 +1,5 @@
+function venv --wraps=python\ -m\ venv\ venv\ /\nsource\ venv/bin/activate.fish --description alias\ venv\ python\ -m\ venv\ venv\ /\nsource\ venv/bin/activate.fish
+    python -m venv venv /
+    source venv/bin/activate.fish
+
+end
