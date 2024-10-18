52e13e88ef760599b59dfa7162fcdc2c95fb5ec6
arkochan <najmussakib@iut-dhaka.edu>
2024-10-18 16:46:12 +0600

    added a csv puglin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -259,4 +259,23 @@ return {
 			vim.keymap.del({ "x", "o" }, "X")
 		end,
 	},
+	{
+		"cameron-wags/rainbow_csv.nvim",
+		config = true,
+		ft = {
+			"csv",
+			"tsv",
+			"csv_semicolon",
+			"csv_whitespace",
+			"csv_pipe",
+			"rfc_csv",
+			"rfc_semicolon",
+		},
+		cmd = {
+			"RainbowDelim",
+			"RainbowDelimSimple",
+			"RainbowDelimQuoted",
+			"RainbowMultiDelim",
+		},
+	},
 }
