ae3b952cf71cb5cd498995ce41a0435d73423cce
arkochan <najmussakibarko@gmail.com>
2025-01-20 21:56:12 +0600

    keybinds TAB

--- nvim_new/lua/plugins/blink.lua
+++ nvim_new/lua/plugins/blink.lua
@@ -1,10 +1,43 @@
 return {
   {
     "Saghen/blink.cmp",
+    -- enabled = false,
+
     opts = {
       keymap = {
-        preset = "enter",
-        ["<tab>"] = { "select_and_accept" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+        -- ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = {
+          -- function(cmp)
+          --   return cmp.accept()
+          -- end,
+          "accept",
+          "snippet_forward",
+          "fallback",
+        },
+        ["<C-Tab>"] = {
+          -- function(cmp)
+          --   return cmp.select_next()
+          -- end,
+          "select_next",
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          -- function(cmp)
+          --   return cmp.select_prev()
+          -- end,
+          "select_prev",
+          "snippet_backward",
+          "fallback",
+        },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+        ["<C-up>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-down>"] = { "scroll_documentation_down", "fallback" },
       },
     },
   },
