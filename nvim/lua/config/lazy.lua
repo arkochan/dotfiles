920edff461d74d63773c3309b95621792238ca72
arkochan <najmussakibarko@gmail.com>
2025-01-27 19:57:26 +0600

    og telescope

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
 
     { import = "plugins" },
   },
