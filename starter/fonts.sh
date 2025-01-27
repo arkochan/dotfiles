dbf158740b24331b28dca3b634667e16ad0f9822
arkochan <najmussakibarko@gmail.com>
2025-01-27 19:59:45 +0600

    emoji font

--- starter/fonts.sh
+++ starter/fonts.sh
@@ -2,6 +2,8 @@
 # Japanese fonts
 # Korean fonts
 # Noto fonts with support for multiple scripts
-sudo pacman -S --noconfirm ttf-indic-otf otf-ipafont noto-fonts-cjk noto-fonts noto-fonts-extra noto-fonts-cjk
+sudo pacman -S --noconfirm ttf-indic-otf otf-ipafont noto-fonts-cjk noto-fonts noto-fonts-extra noto-fonts-cjk ttf-joypixels
 yay -S --noconfirm ttf-jetbrains-mono-nerd
 yay -S --noconfirm ttf-jetbrains-mono
+
+sudo pacman -S
