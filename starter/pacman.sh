6b2c32aee950a6bd86fd3cf735d55273d3c4fdc5
arkochan <najmussakibarko@gmail.com>
2025-01-27 20:00:16 +0600

    bun node go yazi gh

--- starter/pacman.sh
+++ starter/pacman.sh
@@ -1,2 +1,5 @@
-sudo pacman -S --noconfirm cowsay zoxide ripgrep alacritty
+sudo pacman -S --noconfirm cowsay zoxide ripgrep alacritty nodejs npm go yazi github-cli
+curl -fsSL https://bun.sh/install | bash
 yay -S --noconfirm --quiet microsoft-edge-stable-bin
+
+gh apps
