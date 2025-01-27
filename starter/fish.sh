ddd69cfdecc25eb387dd506b88e291a21f821b07
arkochan <najmussakibarko@gmail.com>
2025-01-27 19:59:35 +0600

    lsd fzf zox

--- starter/fish.sh
+++ starter/fish.sh
@@ -4,3 +4,7 @@ chsh -s $(which fish)
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install https://github.com/icezyclon/zoxide.fish/tree/main
+
+pacman --noconfirm -S lsd fzf zoxide
+
+zoxide init fish | source
