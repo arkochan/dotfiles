f1440ab163b4860856c2cdc721d89185b87687bd
arkochan <najmussakibarko@gmail.com>
2025-04-11 23:58:28 +0600

    fast-crawler script

---
+++ fish/functions/fc.fish
@@ -0,0 +1,4 @@
+function fc --wraps=/home/arkochan/Repositories/fast-crawl/result/search_db.sh --description 'alias fc /home/arkochan/Repositories/fast-crawl/result/search_db.sh'
+  /home/arkochan/Repositories/fast-crawl/result/search_db.sh $argv
+        
+end
